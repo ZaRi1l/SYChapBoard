cf47b5035ead0ea2c57012bc663c85ebc7e89cbd
ZaRi1l <zari_09@naver.com>
2025-02-28 15:17:54 +0900

    로그인까지 commit

---
+++ src/main/java/mvc/simple/SimpleController.java
@@ -0,0 +1,62 @@
+package mvc.simple;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SimpleController
+ */
+public class SimpleController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SimpleController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		processRequest(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		processRequest(request, response);
+	}
+
+	private void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String type = request.getParameter("type");
+		
+		Object resultObject = null;
+		if (type == null|| type.equals("greeting")) {
+			resultObject = "안녕하세요.";
+		} else if (type.equals("date")) {
+			resultObject = new java.util.Date();
+		} else {
+			resultObject = "Invalid Type";
+		}
+		
+		
+		request.setAttribute("result", resultObject);
+		
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/chap18/simpleView.jsp");
+		dispatcher.forward(request, response);
+	}
+
+}
