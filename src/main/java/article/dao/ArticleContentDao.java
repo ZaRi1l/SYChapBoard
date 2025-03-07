984f277b1ddc8a062e7692b42b6c896948ea8504
ZaRi1l <zari_09@naver.com>
2025-03-07 15:14:05 +0900

    2025 03 07 commit

--- src/main/java/article/dao/ArticleContentDao.java
+++ src/main/java/article/dao/ArticleContentDao.java
@@ -53,4 +53,13 @@ public class ArticleContentDao {
 		}
 	}
 
+	public int update(Connection conn, int no, String content) throws SQLException {
+		try (PreparedStatement pstmt = conn
+				.prepareStatement("update article_content set content = ? " + "where article_no = ?")) {
+			pstmt.setString(1, content);
+			pstmt.setInt(2, no);
+			return pstmt.executeUpdate();
+		}
+	}
+
 }
